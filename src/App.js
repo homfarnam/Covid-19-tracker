f1cf8814ceddb014da4cc746126b48c2af170ecd
homfarnam <homfarnam@gmail.com>
2020-07-20 23:49:49 +0430

    add api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country , setCountry] = useState('Worldwide')
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // United States , United Knigdom , ...
+            value: country.countryInfo.iso2, // US , UK , FR , ...
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) =>{
+    const countryCode = event.target.value
+    console.log(countryCode);
+    setCountry(countryCode)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+
+        <FormControl className="app__dropdown">
+          <Select onChange={onCountryChange} variant="outlined" value={country}>
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* header */}
+
+      {/*  Title + select input dropdown field */}
+
+      {/* Info Boxes  */}
+      {/* Info Boxes  */}
+      {/* Info Boxes  */}
+
+      {/* table */}
+      {/* Graph */}
+
+      {/* map */}
     </div>
   );
 }
